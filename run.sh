ef9a423627fb6aad23fd1a9f886dfdb2294f845b
cascafico <cascafico@gmail.com>
2018-04-20 14:35:58 +0200

    adj fields in conversion script

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ ADESSO=`date +"%Y-%m-%d"`
 
 wget -nc -O $ADESSO.csv "http://www.dati.salute.gov.it/imgs/C_17_dataset_5_download_itemDownload0_upFile.CSV"
 
-awk -F ";" 'split($3, addr, ",") split($19, lat, ",") split($20, lon, ",") {print $1 ";" lat[1]"."lat[2] ";" lon[1]"."lon[2] ";" addr[1] ";" addr[2]}'
+awk -F ";" 'split($3, addr, ",") split($19, lat, ",") split($20, lon, ",") {print $1 ";" lat[1]"."lat[2] ";" lon[1]"."lon[2] ";" addr[1] ";" addr[2] ";" $6 ";" $8 ";" $15}' $ADESSO.csv > _$ADESSO.csv
 
